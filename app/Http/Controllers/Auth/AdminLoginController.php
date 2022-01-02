83c0e4777f778d275239a4d8bd3cd3e5aa61d88d
hapo-tunght <tung.ht@haposoft.com>
2022-01-02 22:40:53 +0700

    commit before merge

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\JsonResponse;
 
 class AdminLoginController extends Controller
 {
@@ -38,9 +40,16 @@ class AdminLoginController extends Controller
         }
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
-        dd(1);
-        auth()->user()->logout();
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect('/admin/login');
     }
 }
--- resources/views/management/index.blade.php
+++ resources/views/management/index.blade.php
@@ -1,45 +1,48 @@
 @extends('layouts.admin')
 @section('content')
     <div class="account-management">
-        <div class="container-xl d-flex">
-            <ul class="nav nav-pills tab-bar" id="pillsTab" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link active" data-toggle="pill" href="#pillsUser">Users</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-toggle="pill" href="#pillsCourse">Courses</a>
-                </li>
-            </ul>
-            <form class="managementSearch" action="{{ route('management.index') }}" method="GET">
-                <div class="d-flex justify-content-center h-100">
-                    <div class="search-bar">
-                        <input class="search-input" type="text" name="keyword" placeholder="Search...">
-                        <button type="submit" class="search-icon"><i class="fas fa-search"></i></button>
+        <div class="container-fluid admin-navbar d-flex w-100">
+            <div class="row">
+                <ul class="nav nav-pills tab-bar" id="pillsTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-toggle="pill" href="#pillsUser">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="pill" href="#pillsCourse">Courses</a>
+                    </li>
+                </ul>
+                <form class="managementSearch" action="{{ route('management.index') }}" method="GET">
+                    <div class="d-flex justify-content-center h-100">
+                        <div class="search-bar">
+                            <input class="search-input" type="text" name="keyword" placeholder="Search...">
+                            <button type="submit" class="search-icon"><i class="fas fa-search"></i></button>
+                        </div>
                     </div>
-                </div>
-            </form>
-            <ul class="nav nav-pills tab-bar" id="pillsTab" role="tablist">
-                <li class="nav-item">
-                    <a href="{{ route('users.show', [Auth::id()]) }}" class="nav-link">{{ Auth::user()->name }}</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle notification-bell" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        <i class="far fa-bell active">
-                            <span class="notification-number">1<span>
-                        </i>
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-center" aria-labelledby="navbarDropdown" id="dropdownMenuUser">
-                       
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <form id="logoutForm" action="{{ route('admin.logout') }}" method="POST" class="nav-link">
-                        @csrf
-                        <button type="submit" class="" id="logoutButton" class="m-0 text-center">Logout</button>
-                    </form>
-                </li>
-            </ul> 
+                </form>
+                <ul class="nav nav-pills tab-bar" id="pillsTab" role="tablist">
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle notification-bell" href="{{ route('users.show', [Auth::id()]) }}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-center w-100" aria-labelledby="navbarDropdown" id="dropdownMenuUser">
+                            <form id="logoutForm" action="{{ route('admin.logout') }}" method="POST">
+                                @csrf
+                                <button type="submit" id="logoutButton" class="dropdown-item m-0 text-center">Logout</button>
+                            </form>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle notification-bell" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <i class="far fa-bell active">
+                                <span class="notification-number">1<span>
+                            </i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-center" aria-labelledby="navbarDropdown" id="dropdownMenuUser">
+                        
+                        </div>
+                    </li>
+                </ul> 
+            </div>
         </div>
         <div class="tab-content">
             <div class="tab-pane container active" id="pillsUser">
